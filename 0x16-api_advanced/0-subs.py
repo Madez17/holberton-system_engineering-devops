6600c2c1617c8c2b63914b9de567ba10df949973
Madez17 <781@holbertonschool.com>
2019-09-19 21:09:52 +0000

    Pep8 Fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,10 @@ import json
 
 
 def number_of_subscribers(subreddit):
-    API = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(argv[1]))
-    if API.status_code != 200:
-        return 0
-    else:
+    API = requests.get('https://www.reddit.com/r/{}/\
+                       about.json'.format(argv[1]))
+    if API.status_code == 200:
         req = json.loads(API.text)
         return (req.get('data').get('subscribers'))
     else:
+        return 0
