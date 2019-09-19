90b45543fda488e1a4bfd25491f4170785250a98
Madez17 <781@holbertonschool.com>
2019-09-19 21:22:03 +0000

    User_agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,10 @@ import json
 
 
 def number_of_subscribers(subreddit):
+    user_agent = 'madez17'
     API = requests.get('https://www.reddit.com/r/{}/\
-                       about.json'.format(argv[1]))
+                       about.json'.format(argv[1]),
+                       headers={'User-Agent': user_agent})
     if API.status_code == 200:
         req = json.loads(API.text)
         return (req.get('data').get('subscribers'))
