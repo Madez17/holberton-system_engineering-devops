dbece938c79dbf75ed58617d3fa0a29c88fba36d
Madez17 <781@holbertonschool.com>
2019-09-19 20:41:52 +0000

    [Fixed] -  pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,13 @@ import requests
 from sys import argv
 import json
 
+
 def number_of_subscribers(subreddit):
-    API = requests.get('https://www.reddit.com/r/{}/about.json'.format(argv[1]))
+    API = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(argv[1]))
     if API.status_code != 200:
         return 0
     else:
         req = json.loads(API.text)
         return (req.get('data').get('subscribers'))
+    else:
