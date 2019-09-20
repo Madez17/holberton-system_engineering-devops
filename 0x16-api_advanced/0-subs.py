16c0b59e15dc8052cb5f38cd5f9c6140b5295093
Madez17 <47124299+Madez17@users.noreply.github.com>
2019-09-19 23:15:48 -0500

    Fixed code conditions

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,14 @@ import json
 
 def number_of_subscribers(subreddit):
     user_agent = 'Madez'
-    API = requests.get('https://www.reddit.com/r/' + 
-                       subreddit + '/hot.json?limit=10',
+    API = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(argv[1]),
                        headers={'User-Agent': user_agent},
                        allow_redirects=False)
 
-    if API.status_code != 200:
+    if API.status_code == 200:
+        req = json.loads(API.text)     
+        return (req.get('data').get('subscribers'))
+    else:
         return 0
 
-    req = json.loads(API.text)
-    return (req.get('data').get('subscribers'))
