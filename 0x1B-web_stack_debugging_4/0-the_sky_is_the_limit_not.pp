216d29f2b7be626a239fdb75f79b5852336642ad
Madez <781@holbertonschool.com>
2019-10-09 01:44:01 +0000

    [puppet] - create script to replace file unlimit line that execute files

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# Script replace limit to execute file
+exec { 'replace_unlimit':
+  command => "sed -i 's/15/3000/g' /etc/default/nginx",
+  path    => ['usr/bin', 'usr/sbin', '/bin'],
+}
+exec { 'restart-nginx':
+  command => "sudo service nginx restart",
+  path    => ['/usr/bin', '/usr/sbin']
+}
