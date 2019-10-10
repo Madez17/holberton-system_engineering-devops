871f591fabac026e9d98fdfef2dc3949396eac08
Madez17 <47124299+Madez17@users.noreply.github.com>
2019-10-10 15:46:55 -0500

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # Script replace limit to execute file
 exec { 'replace_unlimit':
-  command => "sed -i 's/15/4096/g' /etc/default/nginx",
+  command => "sed -i 's/15/3000/g' /etc/default/nginx",
   path    => ['usr/bin', 'usr/sbin', '/bin'],
-}
+}->
 exec { 'restart-nginx':
   command => 'sudo service nginx restart',
   path    => ['/usr/bin', '/usr/sbin', '/bin']
